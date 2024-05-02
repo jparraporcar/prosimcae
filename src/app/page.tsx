0d0c20b957a89c8b38bfad81338771f4ca09b1b5
jparraporcar <jordiparraporcar@gmail.com>
2024-05-02 21:40:11 +0900

    provisional landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,54 @@
 import { Service } from "@/components/custom/Service";
-import Link from "next/link";
+
+const devData = {
+  items: [
+    "Integrated Fullstack web development",
+    "Server Side Rendering for optimal search engine results",
+    "Single Page Application to obtain the best reactive layout",
+    "RestAPI design and documentation",
+    "Cross-platform mobile app with Ionic",
+    "Mobile native API integration",
+    "Infrastructure on the cloud powered by AWS",
+    "Continuous Deployment/Continuous integration",
+    "Easily mantainable and readable code",
+  ],
+  serviceIntro:
+    "At ProEngineLabs, we specialize in integrated full-stack web development, including server-side rendering for optimal search engine visibility and single-page applications for superior reactive layouts. Our expertise extends to REST API design, comprehensive documentation, and developing cross-platform mobile apps with Ionic. We seamlessly integrate native mobile APIs, and our cloud infrastructure is robustly powered by AWS. With a focus on continuous deployment and integration, we ensure our code is not only easily maintainable but also cleanly readable, setting new standards in software excellence.",
+};
+
+const caeData = {
+  items: [
+    "Infrastructure on the cloud powered by Simscale",
+    "Higly scalable infrastructure",
+    "Higly reliable Solid mechanics simulations with Code Aster",
+    "Wide range of Computer Fluid Dynamics (CFD) simulations with OpenFoam",
+    "Fast CFD simulations using GPU-based solver PaceFish(r)",
+    "Heat transfer effects coupled",
+    "We process your custom material and apply it to our model",
+    "Postprocessing of desired vector, scalar or tensor fields",
+    "Design optimization",
+  ],
+  serviceIntro:
+    "At ProEngineLabs, we deliver excellence in engineering with our cloud-based infrastructure powered by Simscale. Our services include solid mechanics simulations utilizing the robust capabilities of Code Aster, and a comprehensive suite of CFD simulations through OpenFoam. For rapid results, we leverage the GPU-based solver, PaceFish®, ensuring quick and accurate CFD analyses. Our expertise extends to heat transfer effects, custom material processing for model applications, and advanced postprocessing of vector, scalar, or tensor fields. We are committed to driving design optimization, transforming theoretical models into practical solutions.",
+};
 
 const Home: React.FC = () => {
   return (
     <main className="w-full flex flex-row flex-wrap gap-y-20 justify-evenly mt-28">
-      <Service sectionClass="" serviceName="Software Development" />
-      <Service sectionClass="" serviceName="CAE Analysis" />
+      <Service
+        sectionClass=""
+        serviceIntro={devData.serviceIntro}
+        serviceName="Software Development"
+        items={devData.items}
+        hrefDiscover="/dev"
+      />
+      <Service
+        sectionClass=""
+        serviceIntro={caeData.serviceIntro}
+        serviceName="CAE Analysis"
+        items={caeData.items}
+        hrefDiscover="/cae"
+      />
     </main>
   );
 };
--- src/components/custom/Service.tsx
+++ src/components/custom/Service.tsx
@@ -5,50 +5,46 @@ import { FaCheck } from "react-icons/fa";
 interface ServiceProps {
   sectionClass: string;
   serviceName: string;
+  serviceIntro: string;
+  hrefDiscover: string;
+  items: string[];
 }
 
 export const Service: React.FC<ServiceProps> = (props) => {
   return (
     <section
       className={cn([
-        "flex flex-col gap-8 px-4 md:px-6 lg:gap-12",
+        "w-5/12 flex flex-col align-middle gap-8 px-4 md:px-6 lg:gap-12",
         props.sectionClass,
       ])}
     >
-      <div className="flex flex-col items-start justify-center space-y-4">
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl self-center">
-          {props.serviceName}
-        </h2>
-        <div className="rounded-lg border border-gray-200 bg-white p-6 dark:border-gray-800 dark:bg-gray-950">
-          <ul className="space-y-2 text-gray-500 dark:text-gray-400">
-            <li className="flex items-start">
+      <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl self-center">
+        {props.serviceName}
+      </h2>
+      <p>{props.serviceIntro}</p>
+      <div className="rounded-lg border border-gray-200 bg-white p-6 dark:border-gray-800 dark:bg-gray-950">
+        <ul className="space-y-2 text-gray-500 dark:text-gray-400">
+          {props.items.map((item, index) => (
+            <li key={index} className="flex items-start">
               <FaCheck className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 dark:text-gray-400" />
-              <span>Boost productivity with our workflow management tools</span>
+              <span>{item}</span>
             </li>
-            <li className="flex items-start">
-              <FaCheck className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 dark:text-gray-400" />
-              <span>Enhance collaboration across your team</span>
-            </li>
-            <li className="flex items-start">
-              <FaCheck className="mr-2 h-5 w-5 flex-shrink-0 text-gray-500 dark:text-gray-400" />
-              <span>Streamline your operations for maximum efficiency</span>
-            </li>
-          </ul>
-        </div>
-        <div className="w-full flex flex-row justify-evenly">
-          <Link
-            className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-            href="#"
-          >
-            Discover
-          </Link>
-          <Link
-            className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-            href="#"
-          >
-            Contact us
-          </Link>
-        </div>
+          ))}
+        </ul>
+      </div>
+      <div className="w-full flex flex-row justify-evenly">
+        <Link
+          className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          href={props.hrefDiscover}
+        >
+          Discover
+        </Link>
+        <Link
+          className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          href="/contactus"
+        >
+          Contact us
+        </Link>
       </div>
     </section>
   );
