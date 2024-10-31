401624ae8def8af6c951495ca5f24d2bb6fcd7bd
jparraporcar <jordiparraporcar@gmail.com>
2024-10-31 20:23:16 +0100

    refactoring content in landing page and creating new folder for client components - 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navigation } from "@/components/custom/navigation";
+import { Navigation } from "@/components/custom/client/navigation";
 import { Button } from "@/components/ui/button";
 
 const inter = Inter({ subsets: ["latin"] });
