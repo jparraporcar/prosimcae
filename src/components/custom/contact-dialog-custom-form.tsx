fd58d64ed2fa6c60b3335e6d07dccd13b5133e40
jparraporcar <jordiparraporcar@gmail.com>
2024-11-02 20:55:06 +0100

    added field of level of complexity and some flexbox styling in form for custom engineering projects

--- src/components/custom/contact-dialog-custom-form.tsx
+++ src/components/custom/contact-dialog-custom-form.tsx
@@ -15,19 +15,30 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import "./contact-dialog-custom-form.css";
 
 const formSchema = z.object({
   company: z.string().min(4, {
     message: "Company name must be at least 4 characters.",
   }),
-  companyCountry: z.string().min(4, {
+  country: z.string().min(4, {
     message: "Company location name must be at least 4 characters.", // look for collection of states and change to selection component
   }),
-  contactName: z.string().min(6, {
+  contact: z.string().min(6, {
     message: "Contact name must be at least 6 characters.",
   }),
   email: z.string().email({
     message: "Please enter a valid email address.",
+  }),
+  complexity: z.string({
+    required_error: "Please select a level of complexity.",
   }),
 });
 
@@ -36,9 +47,10 @@ export const ContactDialogCustomForm = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       company: "",
-      companyCountry: "",
-      contactName: "",
+      country: "",
+      contact: "",
       email: "",
+      complexity: "",
     },
   });
 
@@ -50,71 +62,109 @@ export const ContactDialogCustomForm = () => {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="company"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Company</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              {/* <FormDescription>
+        <div className="w-full flex flex-row justify-between">
+          <div className="w-52">
+            <FormField
+              control={form.control}
+              name="company"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>
                 Please indicate your company&apos;s full name
               </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="companyCountry"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Company country</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              {/* <FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="w-52">
+            <FormField
+              control={form.control}
+              name="country"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Country</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>
                 Please indicate your company&apos;s country
               </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="contactName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Contact</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              {/* <FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+        <div className="w-full flex flex-row justify-between">
+          <div className="w-52">
+            <FormField
+              control={form.control}
+              name="contact"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contact</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>
                 Please indicate you name and surname
               </FormDescription> */}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              {/* <FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="w-52">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>
                 Please indicate your corporate email
               </FormDescription> */}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+        <FormField
+          control={form.control}
+          name="complexity"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Project complexity</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Project complexity" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="Low">Low</SelectItem>
+                  <SelectItem value="Middle">Middle</SelectItem>
+                  <SelectItem value="High">High</SelectItem>
+                </SelectContent>
+              </Select>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <div className="w-full flex flex-row justify-end">
+          <Button className="mr-0" type="submit">
+            Submit
+          </Button>
+        </div>
       </form>
     </Form>
   );
