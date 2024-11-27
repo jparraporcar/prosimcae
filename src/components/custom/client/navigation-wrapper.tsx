ea0ef7b4418205e0ede3b37ad01682395aced01b
jparraporcar <jordiparraporcar@gmail.com>
2024-11-27 16:27:29 +0900

    testing sheet in mobile - 3

--- src/components/custom/client/navigation-wrapper.tsx
+++ src/components/custom/client/navigation-wrapper.tsx
@@ -1,34 +1,18 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { NavigationMobile } from "./navigation-mobile";
 import { Navigation } from "./navigation";
+import { NavigationMobile } from "./navigation-mobile";
 
 export const NavigationWrapper: React.FC = () => {
-  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  return (
+    <div>
+      {/* Desktop navigation - Hidden on mobile */}
+      <div className="hidden md:block">
+        <Navigation />
+      </div>
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 767);
-    };
-
-    // Run on initial mount
-    handleResize();
-
-    // Add event listener for resizing
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup event listener on unmount
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  if (isMobile === null) {
-    console.log(isMobile);
-    // Render nothing or a placeholder during the initial load
-    return null;
-  }
-
-  return isMobile ? <NavigationMobile /> : <Navigation />;
+      {/* Mobile navigation - Hidden on desktop */}
+      <div className="block md:hidden">
+        <NavigationMobile />
+      </div>
+    </div>
+  );
 };
