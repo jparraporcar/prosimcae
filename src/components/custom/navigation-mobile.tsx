a121f7a3a1a33bbfb897060dea54e2fb8d6546bd
jparraporcar <jordiparraporcar@gmail.com>
2024-11-29 17:32:50 +0900

    fixed the background colour of a predetermined active services button in mobile menu - 6

--- src/components/custom/navigation-mobile.tsx
+++ src/components/custom/navigation-mobile.tsx
@@ -13,6 +13,7 @@ import { Menu, X } from "lucide-react";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import "./navigation-mobile.css";
 
 export const NavigationMobile: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,31 +29,34 @@ export const NavigationMobile: React.FC = () => {
         <Menu size={34} onClick={() => setIsOpen(true)} />
       </SheetTrigger>
       <SheetContent>
-        <div className="flex flex-col mt-8 ml-4 gap-y-6 text-xl">
-          <Button
+        <div
+          id="mobile-nav-container"
+          className="flex flex-col mt-8 ml-4 gap-y-6 text-xl"
+        >
+          <button
             onClick={() => handleClick("#main-section-title-services")}
             className="text-black bg-white inline-flex border-white h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-white focus:outline-none focus:border-white -webkit-appearance-none"
           >
             Services
-          </Button>
-          <Button
+          </button>
+          <button
             onClick={() => handleClick("#main-section-title-technology")}
             className="text-black bg-white inline-flex border-white h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-white focus:outline-none focus:border-white -webkit-appearance-none"
           >
             Technology
-          </Button>
-          <Button
+          </button>
+          <button
             onClick={() => handleClick("#main-section-title-study-cases")}
             className="text-black bg-white inline-flex border-white h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-white focus:outline-none focus:border-white -webkit-appearance-none"
           >
             Study Cases
-          </Button>
-          <Button
+          </button>
+          <button
             onClick={() => handleClick("#main-section-title-collaborations")}
             className="text-black bg-white inline-flex border-white h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-white focus:outline-none focus:border-white -webkit-appearance-none"
           >
             Collaborations
-          </Button>
+          </button>
         </div>
         <SheetClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800">
           <X className="h-4 w-4" onClick={() => setIsOpen(false)} />
