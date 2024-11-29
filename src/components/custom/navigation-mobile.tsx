a9a0771a3116c77627c142610058cfc63f485bb6
jparraporcar <jordiparraporcar@gmail.com>
2024-11-29 17:13:01 +0900

    fixed the background colour of a predetermined active services button in mobile menu - 4

--- src/components/custom/navigation-mobile.tsx
+++ src/components/custom/navigation-mobile.tsx
@@ -31,25 +31,25 @@ export const NavigationMobile: React.FC = () => {
         <div className="flex flex-col mt-8 ml-4 gap-y-6 text-xl">
           <Button
             onClick={() => handleClick("#main-section-title-services")}
-            className="text-black bg-white inline-flex border-transparent h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-transparent"
+            className="text-black bg-white inline-flex border-transparent h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-transparent focus:outline-none focus:border-transparent -webkit-appearance-none"
           >
             Services
           </Button>
           <Button
             onClick={() => handleClick("#main-section-title-technology")}
-            className="text-black bg-white inline-flex border-transparent  h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-transparent"
+            className="text-black bg-white inline-flex border-transparent h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-transparent focus:outline-none focus:border-transparent -webkit-appearance-none"
           >
             Technology
           </Button>
           <Button
             onClick={() => handleClick("#main-section-title-study-cases")}
-            className="text-black bg-white inline-flex border-transparent  h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-transparent"
+            className="text-black bg-white inline-flex border-transparent h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-transparent focus:outline-none focus:border-transparent -webkit-appearance-none"
           >
             Study Cases
           </Button>
           <Button
             onClick={() => handleClick("#main-section-title-collaborations")}
-            className="text-black bg-white inline-flex border-transparent  h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-transparent"
+            className="text-black bg-white inline-flex border-transparent h-10 w-max items-center justify-center rounded-md bg-transparent px-4 text-lg font-medium py-4 pr-4 active:bg-slate-300 active:border-transparent focus:outline-none focus:border-transparent -webkit-appearance-none"
           >
             Collaborations
           </Button>
