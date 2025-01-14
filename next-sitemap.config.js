1ed9e114c34b97ef808e537d71681f3c0a46d2c7
jparraporcar <jordiparraporcar@gmail.com>
2025-01-14 19:08:13 +0900

    set up sitemap and related google console config

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://www.prosimcae.com",
+  generateRobotsTxt: true, // Automatically generate robots.txt
+  // Additional options
+};
