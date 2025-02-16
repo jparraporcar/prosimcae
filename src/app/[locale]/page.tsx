0d8e7c4b54bfe4b29f92c2230fd0d76acf51a0aa
jparraporcar <jordiparraporcar@gmail.com>
2025-02-16 12:56:19 +0900

    fixed issue of margin in rotatative carousel affecting the mobile version

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -63,7 +63,7 @@ const Home = () => {
           <h2 className="text-3xl max-md:text-2xl font-bold text-gray-900 dark:text-gray-50">
             {t(customEngineering.serviceName)}
           </h2>
-          <div className="mr-32">
+          <div className="mr-32 max-md:mr-0">
             <CarouselWrapper images={openImages} />
           </div>
         </div>
@@ -71,7 +71,7 @@ const Home = () => {
           <h2 className="text-3xl max-md:text-2xl font-bold text-gray-900 dark:text-gray-50">
             {t(onDemandEngineering.serviceName)}
           </h2>
-          <div className="mr-32">
+          <div className="mr-32 max-md:mr-0">
             <CarouselWrapper images={comImages} />
           </div>
         </div>
