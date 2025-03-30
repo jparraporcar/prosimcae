a4525091562e5d166888a8409002d0253458e5c6
jparraporcar <jordiparraporcar@gmail.com>
2025-03-30 16:40:42 +0900

    added clarity cookie

--- src/components/custom/client/analytics-provider.tsx
+++ src/components/custom/client/analytics-provider.tsx
@@ -8,22 +8,27 @@ export const AnalyticsProvider = () => {
 
   return (
     <>
-      {cookies.analyticsConsent && (
-        <>
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-CKC5FPSJWE"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-CKC5FPSJWE"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-CKC5FPSJWE');
             `}
-          </Script>
-        </>
-      )}
+      </Script>
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "qw79yp8ekc");
+        `}
+      </Script>
     </>
   );
 };
